5a5c07350208a38537f6986ec7fe07b029e3bc12
Jingwen Owen Ou <jingweno@gmail.com>
2014-07-24 08:31:29 -0700

    Read Procfile properly

--- src/main/java/gaffer/core/Procfile.java
+++ src/main/java/gaffer/core/Procfile.java
@@ -1,14 +1,41 @@
 package gaffer.core;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Procfile {
+	private static final Pattern PROCFILE_REGEXP_PATTERN = Pattern
+			.compile("^([A-Za-z0-9_]+):\\s*(.+)$");
+	private List<ProcfileEntry> entries = new LinkedList<ProcfileEntry>();
+
 	public ProcfileEntry[] getEntries() {
-		return new ProcfileEntry[] {
-				new ProcfileEntry("web", "./script/server"),
-				new ProcfileEntry("worker", "bundle exec rake jobs:work") };
+		return entries.toArray(new ProcfileEntry[entries.size()]);
 	}
 
-	public static Procfile read(String dir2) {
-		return new Procfile();
+	private void addEntry(ProcfileEntry entry) {
+		entries.add(entry);
+	}
+
+	public static Procfile read(String dir) throws IOException {
+		Path path = Paths.get(dir, "Procfile");
+		List<String> lines = Files.readAllLines(path);
+
+		Procfile procfile = new Procfile();
+		for (String line : lines) {
+			Matcher matcher = PROCFILE_REGEXP_PATTERN.matcher(line);
+			if (matcher.matches()) {
+				ProcfileEntry entry = new ProcfileEntry(matcher.group(1),
+						matcher.group(2));
+				procfile.addEntry(entry);
+			}
+		}
+
+		return procfile;
 	}
 }
---
+++ src/test/java/gaffer/core/ProcfileTest.java
@@ -0,0 +1,28 @@
+package gaffer.core;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class ProcfileTest {
+	@Test
+	public void readProcfile() throws IOException, URISyntaxException {
+		Path path = Paths.get(File.separator + "fixtures");
+		URL url = getClass().getResource(path.toString());
+		Procfile procfile = Procfile.read(url.getFile());
+		ProcfileEntry[] entries = procfile.getEntries();
+
+		assertEquals(2, entries.length);
+		assertEquals("web", entries[0].getName());
+		assertEquals("./script/server", entries[0].getCommand());
+		assertEquals("worker", entries[1].getName());
+		assertEquals("rake jobs:work", entries[1].getCommand());
+	}
+}
