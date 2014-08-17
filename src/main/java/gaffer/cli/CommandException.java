b18bb014edae1544bb2354b98e725b6006cd46e0
Jingwen Owen Ou <jingweno@gmail.com>
2014-08-17 13:59:53 -0700

    Reformat

--- src/main/java/gaffer/cli/CommandException.java
+++ src/main/java/gaffer/cli/CommandException.java
@@ -2,7 +2,7 @@ package gaffer.cli;
 
 public class CommandException extends Exception {
   private static final long serialVersionUID = 974553763738904807L;
-  
+
   public CommandException(String msg) {
     super(msg);
   }
